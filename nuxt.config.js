3d6a668febc895769987fb63817676c5763fa830
Tarik02 <Taras.Fomin@gmail.com>
2018-09-18 18:23:54 +0300

    Add env logic

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,32 +1,30 @@
-const env = {
-  API_HTTPS: false,
-  API_HOST: '192.168.1.10',
-  API_PORT: 8080,
-  API_PREFIX: '/api/v1/',
-
-  UPLOADS_URL: 'http://192.168.1.10:8080/',
-}
+const dotenv = require('dotenv')
+const env = dotenv.config().parsed
 
 module.exports = {
   env,
 
   mode: 'spa',
-  
-  /*
-  ** Headers of the page
-  */
+
+  render: {
+    ssr: false,
+  },
+
   head: {
-    title: 'restaurant',
+    title: 'Restaurant',
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no' },
-      { hid: 'description', name: 'description', content: 'Young informatics 2018' }
+      { hid: 'description', name: 'description', content: 'Young informatics 2018' },
     ],
+
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
-    ]
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
+    ],
   },
+
   modules: [
     ['@nuxtjs/axios', {
       https: env.API_HTTPS,
@@ -34,9 +32,11 @@ module.exports = {
       port: env.API_PORT,
       prefix: env.API_PREFIX,
     }],
+
     ['@nuxtjs/toast', {
       duration: 2500,
     }],
+
     ['@nuxtjs/pwa', {
       meta: {
         nativeUI: true,
@@ -48,6 +48,7 @@ module.exports = {
       },
     }],
   ],
+
   plugins: [
     '~/plugins/vuetify.js',
 
@@ -59,33 +60,29 @@ module.exports = {
     '~/plugins/currency.js',
     '~/plugins/uploaded.js',
   ],
+
   css: [
     '~/assets/style/app.styl',
   ],
-  /*
-  ** Customize the progress bar color
-  */
+  
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
+  
   build: {
     vendor: [
       '~/plugins/vuetify.js',
     ],
+
     extractCSS: true,
-    /*
-    ** Run ESLint on save
-    */
+    
     extend (config, ctx) {
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
         })
       }
-    }
-  }
+    },
+  },
 }
