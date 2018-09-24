7f5bd1cf748b442f3c7acb2562fba2d222e40754
Tarik02 <Taras.Fomin@gmail.com>
2018-09-24 21:50:56 +0300

    Debugging

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,12 +3,14 @@ export default function ({ $axios, store }) {
     const account = store.getters['accounts/current']
     if (account !== null) {
       const {tokenType, accessToken} = account.auth
-      
-      if (null !== config.headers['Authorization']) {
-        config.headers['Authorization'] = `${tokenType} ${accessToken}`
-      } else {
+
+      console.log('before', config.url, { ...config.headers })
+      if (null === config.headers['Authorization']) {
         delete config.headers['Authorization']
+      } else {
+        config.headers['Authorization'] = `${tokenType} ${accessToken}`
       }
+      console.log('after', config.url, { ...config.headers })
     }
 
     return config
