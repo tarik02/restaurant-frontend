2855efa8f96fc73eda6cbfb9dcebfe7620d01c56
Tarik02 <Taras.Fomin@gmail.com>
2018-10-26 21:38:00 +0300

    Speedup

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="container">
+  <div v-if="showError" class="container">
     <h1 v-if="error.statusCode === 404">Дана сторінка не існує</h1>
     <h1 v-else>Виникла помилка</h1>
     <nuxt-link to="/">Головна сторінка</nuxt-link>
   </div>
+  <div v-else />
 </template>
 
 <script>
 export default {
   layout: 'menuless',
+
   props: {
     error: { type: null, required: true },
   },
+
+  data: () => ({
+    showError: true,
+  }),
 
   created() {
     const { error } = this
@@ -20,14 +26,19 @@ export default {
       return
     }
 
+    this.showError = false
+
     if (
       parseInt(error.response.status) === 307 &&
       error.response.statusText === 'not-installed'
     ) {
       if (this.$route.name !== 'install') {
         this.$router.replace({ name: 'install' })
+        return
       }
     }
+
+    this.showError = true
   },
 }
 </script>
